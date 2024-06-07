2817f42bcdfb1cb95c7b54291b9877604d46ecba
gedda68 <gaitchis@gmail.com>
2024-06-07 15:18:36 +1000

    added logon/logout actions

--- website/views.py
+++ website/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home (request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #authenticate 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            context ={'alert' : 'success' }
+            
+            return render(request, 'home.html', context)
+        else:
+            messages.success(request, "There was an error logging, please try again...")
+            context ={'alert' : 'warning' }
+            
+            return render(request, 'home.html', context)
+
+    else:        
+        return render(request, 'home.html', {})
+
+# def login_user(request):
+#     pass
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have now been logged out...')
+    context ={'alert' : 'success' }
+    return render(request, 'home.html', context)
